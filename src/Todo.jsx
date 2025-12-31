c3110d2d96aefcc6644a7c27780c87339cb7689c
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2025-12-31 17:12:14 +0900

    fix:Reactでの実装を意識したモックに変更

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,6 +1,11 @@
 import "./styles.css";
+import { useState } from "react";
 
 export const Todo = () => {
+    //useStateの初期化
+    const [incompleteTodos, setIncompleteTodos] = useState(["Todoです1", "Todoです2"]);
+    const [completeTodos, setCompleteTodos] = useState(["Todoでした1", "Todoでした2"]);
+
     return (
         <>
             <div className="input-area">
@@ -10,25 +15,31 @@ export const Todo = () => {
             <div className="incomplete-area">
                 <p className="title">未完了のTodo</p>
                 <ul>
-                    <li>
-                        <div className="list-row">
-                            <p className="todo-item">Todoです</p>
-                            <button>完了</button>
-                            <button>削除</button>
-                        </div>
-                    </li>
+                    {/* //未完了のTodoリストをmap関数を用いて表示 */}
+                    {incompleteTodos.map((todo) => (
+                            // key は React がリスト要素を一意に識別し、追加・削除・並び替え時の差分更新を正しく行うために必要
+                            <li key={todo}>
+                                <div className="list-row">
+                                    <p className="todo-item">{todo}</p>
+                                    <button>完了</button>
+                                    <button>削除</button>
+                                </div>
+                            </li>
+                    ))}
                 </ul>
             </div>
             <div className="complete-area">
                 <p className="title">完了したTodo</p>
                 <ul>
-                    <li>
-                        
-                        <div className="list-row">
-                            <p className="todo-item">完了したTodoです</p>
-                            <button>戻す</button>
-                        </div>
-                    </li>
+                    {/* //完了したTodoリストをmap関数を用いて表示 */}
+                    {completeTodos.map((todo) => (
+                        <li key={todo}>
+                            <div className="list-row">
+                                <p className="todo-item">{todo}</p>
+                                <button>戻す</button>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </>
