3b714ed2bc7db4c215a56ac40a5c97fb3c9d7605
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2025-12-31 16:47:13 +0900

    fix:リニューアル部分のベースを作成

---
+++ src/Todo.jsx
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+export const Todo = () => {
+    return (
+        <>
+            <div className="input-area">
+                <input placeholder="TODOを入力" />
+                <button>追加</button>
+            </div>
+            <div className="incomplete-area">
+                <p className="title">未完了のTodo</p>
+                <ul>
+                    <li>
+                        <div className="list-row">
+                            <p className="todo-item">Todoです</p>
+                            <button>完了</button>
+                            <button>削除</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="complete-area">
+                <p className="title">完了したTodo</p>
+                <ul>
+                    <li>
+                        
+                        <div className="list-row">
+                            <p className="todo-item">完了したTodoです</p>
+                            <button>戻す</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+}
